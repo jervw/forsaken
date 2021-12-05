d339393463ef3e82bf6e80576ee65504cbf75a00
jervw <vuolajere@gmail.com>
2021-12-05 17:51:32 +0200

    lobby ui

--- Assets/scripts/Lobby.cs
+++ Assets/scripts/Lobby.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using Photon.Pun;
 using Photon.Realtime;
 
@@ -8,9 +10,11 @@ namespace Com.Jervw.Crimson
 {
     public class Lobby : MonoBehaviourPunCallbacks
     {
+        TMP_Text roomName;
+        TMP_InputField roomNameInput;
+
         [SerializeField]
         private byte maxPlayersPerRoom = 4;
-
 
         string gameVersion = "0.1";
 
@@ -22,6 +26,9 @@ namespace Com.Jervw.Crimson
         // Start is called before the first frame update
         void Start()
         {
+            roomName = GameObject.Find("RoomName").GetComponent<TMP_Text>();
+            roomNameInput = GameObject.Find("InputField").GetComponent<TMP_InputField>();
+            Connect();
         }
 
         // Update is called once per frame
@@ -33,36 +40,55 @@ namespace Com.Jervw.Crimson
         public void OfflineMode()
         {
             Debug.Log("offline mode");
-
             PhotonNetwork.OfflineMode = true;
             PhotonNetwork.CreateRoom("Offline");
         }
 
+
+        public void CreateLobby()
+        {
+            if (!PhotonNetwork.IsConnected) return;
+
+            if (!PhotonNetwork.InRoom)
+            {
+                Debug.Log("CreateLobby()");
+                string roomId = Random.Range(1000, 9999).ToString();
+                PhotonNetwork.CreateRoom(roomId, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+                roomName.text = roomId;
+                GameObject.Find("Main").SetActive(false);
+            }
+        }
+
+        public void JoinLobby()
+        {
+            if (!PhotonNetwork.IsConnected) return;
+
+            if (!PhotonNetwork.InRoom)
+            {
+                Debug.Log("JoinLobby()");
+                PhotonNetwork.JoinRoom(roomNameInput.text);
+            }
+        }
+
+
         public void Connect()
         {
-            //Debug.Log("Connecting to server");
-
-            if (PhotonNetwork.IsConnected)
-                PhotonNetwork.JoinRandomRoom();
-            else
-            {
-                PhotonNetwork.GameVersion = gameVersion;
-                PhotonNetwork.ConnectUsingSettings();
-            }
+            Debug.Log("Connect()");
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
         }
 
         public override void OnJoinRandomFailed(short returnCode, string message)
         {
-            //Debug.Log("No existing rooms, creating a new room");
-            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+            Debug.Log(message);
+            //PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
         }
 
         public override void OnConnectedToMaster()
         {
             if (!PhotonNetwork.OfflineMode)
             {
-                //Debug.Log("Connected to master");
-                PhotonNetwork.JoinRandomRoom();
+                Debug.Log("Connected to master");
             }
 
         }
@@ -76,7 +102,10 @@ namespace Com.Jervw.Crimson
         {
             //Debug.Log("Joined room");
             if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
-                PhotonNetwork.LoadLevel(1);
+            {
+                //PhotonNetwork.LoadLevel(1);
+
+            }
 
         }
     }
